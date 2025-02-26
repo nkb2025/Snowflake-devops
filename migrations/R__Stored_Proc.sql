3e194c408e77dd285b0516a642d6b4858d8311a7
Tushar Zaware <tushar.zaware@emeal.nttdata.com>
2025-02-26 17:02:12 +0000

    update R script

--- migrations/R__Stored_Proc.sql
+++ migrations/R__Stored_Proc.sql
@@ -5,8 +5,5 @@ AS
 $$
   var today = new Date();
   var yyyy = today.getFullYear();
-  var mm = String(today.getMonth() + 1).padStart(2, '0');
-  var dd = String(today.getDate()).padStart(2, '0');
-  var todayStr = yyyy + '-' + mm + '-' + dd;
   return todayStr;
 $$;
