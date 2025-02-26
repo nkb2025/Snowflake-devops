60053d1be074d2c23fc0364911e7cb7679a7cb8d
Tushar Zaware <tushar.zaware@emeal.nttdata.com>
2025-02-26 16:58:25 +0000

    updated sp

--- migrations/R__Stored_Proc.sql
+++ migrations/R__Stored_Proc.sql
@@ -4,5 +4,9 @@ LANGUAGE JAVASCRIPT
 AS
 $$
   var today = new Date();
-  return today;
+  var yyyy = today.getFullYear();
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var dd = String(today.getDate()).padStart(2, '0');
+  var todayStr = yyyy + '-' + mm + '-' + dd;
+  return todayStr;
 $$;
